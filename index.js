35850668960515fed4b3dae93c6b86baa919bfe3
ashwinjchhetri <aswinaryal@gmail.com>
2021-07-13 17:13:00 +0545

    simple api endpoint introduced to check api status

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/api/v1/status", (req, res, next) => {
+  res.status(200).json({
+    statu: "OK"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`server started at port ${PORT}`);
 });
